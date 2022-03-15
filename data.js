838d2b7cdd117680b39ed482691dd40fa8a5f138
Samrudhi Patil <84308385+Samrudhipatil51@users.noreply.github.com>
2022-03-15 22:47:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+let movies = [
+      {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/silder2.jpg'
+      },
+      {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/loki.jpeg'
+      },
+      {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/silder3.webp'
+      }
+]
